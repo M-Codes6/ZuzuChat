fb32b131a355843a9004299ba3766fd7a06066ce
Muzamil Mushtaq <naikmuzamil06@gmail.com>
2024-11-13 13:51:05 +0530

    Added files for PWA

--- home/home.js
+++ home/home.js
@@ -7,3 +7,19 @@ function playSound() {
     const sound = document.getElementById("zuzuSound");
     sound.play();
 }
+
+
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/service-worker.js")
+        .then(registration => {
+          console.log("Service Worker registered with scope:", registration.scope);
+        })
+        .catch(error => {
+          console.error("Service Worker registration failed:", error);
+        });
+    });
+  }
+  
