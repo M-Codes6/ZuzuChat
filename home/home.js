aebb696c3935d0a970e21d05eeea4addb2246358
Muzamil Mushtaq <naikmuzamil06@gmail.com>
2024-11-15 15:32:35 +0530

    Changed audio setting

--- home/home.js
+++ home/home.js
@@ -3,9 +3,9 @@ document.querySelector('.chat-button').addEventListener('click', () => {
     window.location.href = '../chat/chat.html'; 
 });
 
-function playSound() {
-    const sound = document.getElementById("zuzuSound");
-    sound.play();
+function playZuzuSound() {
+    const audio = document.getElementById("zuzuSound");
+    audio.play();
 }
 
 
